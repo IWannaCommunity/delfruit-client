53b79f189c6a5e1a50a022d3c43e85385ab51d0d
Starz0r <starz0r@starz0r.com>
2023-04-18 13:23:52 -0500

    components/tag: font changes and hover colors

--- src/components/games/tag.jsx
+++ src/components/games/tag.jsx
@@ -1,5 +1,9 @@
 export default function Tag({ name }) {
     return (
-        <div className="box-border flex flex-row justify-center items-center px-2 border-[1px] rounded-[20px] bg-[#F9F9F9] border-[#232123] h-[24px] cursor-pointer hover:bg-[#FFFFCC] group gap-3">{name}</div>
+        <div className="box-border flex flex-row justify-center items-center px-2 gap-2.5 border rounded-[20px] bg-[#F9F9F9] border-[#232123] h-[24px] border-solid cursor-pointer group transition ease-linear delay-100 hover:invert">
+            <p className="order-none grow-0 font-normal not-italic font-['Prompt'] text-base items-end text-center capitalize">
+                {name}
+            </p>
+        </div>
     );
 }
