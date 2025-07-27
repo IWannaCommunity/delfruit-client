a4716db9870a515c00980809f5ee029af80185fd
Starz0r <starz0r@starz0r.com>
2025-07-26 22:31:30 -0400

    pages/admin-dashboard: submit edited game detail changes

--- src/pages/admin-dashboard/games/index.tsx
+++ src/pages/admin-dashboard/games/index.tsx
@@ -1,10 +1,73 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { GamesApi } from "../../../generated/swagger-codegen";
-import { useEffect, useState } from "react";
+import { Game, GamesApi } from "../../../generated/swagger-codegen";
+import { FormEvent, useEffect, useState } from "react";
 import { times } from "lodash";
 
-function AdminDashboard(): JSX.Element {
+function EditPanel(props: { data: Game }): JSX.Element {
+	async function saveChanges(evt: FormEvent<HTMLFormElement>) {
+		evt.preventDefault();
+
+		const client = new GamesApi(void 0, "http://localhost:4201");
+
+		const formdata = new FormData(evt.currentTarget);
+
+		await client.patchGame(formdata as Game, "", props.data.id);
+	}
+
+	return (
+		<>
+			<form onSubmit={saveChanges}>
+				<label>
+					Game Name
+					<input type="text" name="game" />
+				</label>
+				<label>
+					URL
+					<input type="url" name="url" />
+				</label>
+				<label>
+					Author
+					<input type="text" name="author_raw" />
+				</label>
+				<label>
+					Collab
+					<input type="checkbox" name="collab" />
+				</label>
+				<button type="submit">
+					<label>Save Changes</label>
+				</button>
+			</form>
+		</>
+	);
+}
+
+function GameEntry(props: { details: Game }): JSX.Element {
+	const [showEditPanel, setShowEditPanel] = useState(false);
+
+	return (
+		<p key={props.details.id}>
+			{props.details.name}
+			<div></div>
+			<button
+				id={`${props.details.id}`}
+				type="button"
+				onClick={async (
+					evt: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+				) => {
+					evt.preventDefault();
+					setShowEditPanel(!showEditPanel);
+				}}
+			>
+				update 🛠
+			</button>
+			<div>{showEditPanel && <EditPanel data={props.details} />}</div>
+			<div>&nbsp;</div>
+		</p>
+	);
+}
+
+export default function AdminDashboardGames(): JSX.Element {
 	const router = useRouter();
 
 	const [games, setGames] = useState([]);
@@ -35,7 +98,7 @@ function AdminDashboard(): JSX.Element {
 			);
 			let elems: Array<JSX.Element> = new Array(req.data.length);
 			for (const game of req.data) {
-				elems.push(<p key={game.id}>{game.name}</p>);
+				elems.push(<GameEntry details={game} />);
 			}
 			setGames(elems);
 		};
@@ -53,5 +116,3 @@ function AdminDashboard(): JSX.Element {
 		</div>
 	);
 }
-
-export default AdminDashboard;
