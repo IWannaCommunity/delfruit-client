1a041e61288d03ebbc57e710ea9187538e4137d6
TTBB <ttbbaiwana@gmail.com>
2025-08-14 21:00:54 -0400

    components/home/games: skeleton games component for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
 import Head from "next/head";
 import type { AnyElem } from "../utils/element";
 import Header from "../components/header";
+import News from "../components/home/news";
+import Games from "../components/home/games";
+import Reviews from "../components/home/reviews";
 
 export default function Home(): JSX.Element {
 	return (
-		<>
+		<div>
 			<Head>
 				<title>Delicious Fruit</title>
 			</Head>
 			<body>
 				<div id="container">
 					<Header />
+					<div id="content">
+						<News />
+						<Games />
+						<Reviews />
+					</div>
 				</div>
 			</body>
-		</>
+		</div>
 	);
 }
