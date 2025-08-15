579141deef157eafe429bd8c506e66d4e73e6c21
unknown <khaiquocbui@gmail.com>
2025-08-15 03:38:28 -0400

    Separated game/review and gamelist/reviewlist components

---
+++ src/components/game.tsx
@@ -0,0 +1,13 @@
+export default function Game(): JSX.Element {
+	return (
+		<tr>
+			<td>
+				<a href="/game">I wanna be the Blank</a>
+			</td>
+			<td className="rating">Aug 13, 2025</td>
+			<td className="rating">100</td>
+			<td className="rating">10</td>
+			<td className="rating">47</td>
+		</tr>
+	);
+}
