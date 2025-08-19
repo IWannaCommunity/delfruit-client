054444e4d0b35c192468640b86a4477b08587ea9
Starz0r <starz0r@starz0r.com>
2025-08-18 22:57:14 -0400

    next: transpile our API SDK so it's usable in the project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "192.168.0.13",
+				port: "9000",
+				pathname: "**",
+			},
+		],
+	},
+	transpilePackages: ["delfruit-swagger-cg-sdk"],
+	webpack: (cfg) => {
+		cfg.module.rules?.push({
+			test: new RegExp("/src/pages\/v2/"),
+			loader: "ignore-loader",
+		});
+		return cfg;
+	},
+	output: "standalone",
+};
 
-module.exports = nextConfig
+const withTM = require('next-transpile-modules')(["delfruit-swagger-cg-sdk"]); // TODO: remove this once NextJS stops complaining about it
+
+module.exports = withTM(nextConfig);
+
