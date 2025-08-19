fa2ae862af88513a3b623ed99cf19470a12b8861
Starz0r <starz0r@starz0r.com>
2025-08-19 00:44:34 -0400

    pages/home: fetch newest fangames

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,28 @@ export default function Home(): JSX.Element {
 
 	useMemo(() => {
 		(async () => {
-			let resp = await apiClient.getGamesWithRatings();
+			let resp = await apiClient.getGamesWithRatings(
+				void 0,
+				void 0,
+				false,
+				void 0,
+				void 0,
+				void 0,
+				void 0,
+				void 0,
+				new Date(Date.now() - 30 * 24 * 60 * 60 * 1000), // 30 days ago
+				new Date(Date.now()),
+				void 0,
+				void 0,
+				void 0,
+				void 0,
+				void 0,
+				void 0,
+				0,
+				25,
+				"date_created",
+				"desc",
+			);
 			const gameProps: Array<GameProps> = new Array(resp.data.length);
 			for (const game of resp.data) {
 				gameProps.push({
