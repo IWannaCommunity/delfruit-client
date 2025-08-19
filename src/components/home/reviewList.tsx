429077c99e79d47809bfd77b5d7f05fa9958d138
Starz0r <starz0r@starz0r.com>
2025-08-19 03:54:50 -0400

    components/home/reviewList: use passed in props instead of mock data

--- src/components/home/reviewList.tsx
+++ src/components/home/reviewList.tsx
@@ -1,17 +1,33 @@
 import Whitespace from "../whitespace";
 import Review from "../review";
+import { Review as ReviewT } from "delfruit-swagger-cg-sdk";
 
-export default function ReviewList(): JSX.Element {
+type ReviewListProps = {
+	reviews: ReviewT[];
+};
+
+export default function ReviewList(props: ReviewListProps): JSX.Element {
 	return (
 		<div>
 			<h2>Latest Reviews</h2>
 			<p className="notes">Showing 5 of 115447</p>
-			<Review />
-			<Review />
-			<Review />
-			<Review />
-			<Review />
-			<a className="standalone" href="/">Read more reviews!</a>
+			{props.reviews.map((review) => {
+				return (
+					<Review
+						author={review.user_name}
+						comment={review.comment}
+						rating={review.rating}
+						difficulty={review.difficulty}
+						tags={review.tags}
+						dateCreated={review.dateCreated}
+						likes={review.like_count}
+					/>
+				);
+			})}
+			<a className="standalone" href="/">
+				Read more reviews!
+			</a>
 		</div>
 	);
 }
+
