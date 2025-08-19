577cd92368de134e31690a1bd2f094106c7e5b7b
Starz0r <starz0r@starz0r.com>
2025-08-19 03:55:27 -0400

    pages/home: display real reviews

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,56 +3,71 @@ import Header from "../components/header";
 import News from "../components/home/news";
 import GameList from "../components/home/gameList";
 import ReviewList from "../components/home/reviewList";
-import { CompositeApi } from "delfruit-swagger-cg-sdk";
+import { CompositeApi, Review, ReviewsApi } from "delfruit-swagger-cg-sdk";
 import { useMemo, useState } from "react";
 import { GameProps } from "../components/game";
 import type { Config } from "../utils/config";
 
 const CFG: Config = require("../config.json");
 
-const apiClient: CompositeApi = new CompositeApi(void 0, CFG.apiURL.toString());
+const COMPOSITEAPICLIENT: CompositeApi = new CompositeApi(
+	void 0,
+	CFG.apiURL.toString(),
+);
+const REVIEWSAPICLIENT: ReviewsApi = new ReviewsApi(
+	void 0,
+	CFG.apiURL.toString(),
+);
 
 export default function Home(): JSX.Element {
 	const [games, setGames] = useState<GameProps[]>([]);
+	const [reviews, setReviews] = useState<Review[]>([]);
 
 	useMemo(() => {
-		(async () => {
-			let resp = await apiClient.getGamesWithRatings(
-				void 0,
-				void 0,
-				false,
-				void 0,
-				void 0,
-				void 0,
-				void 0,
-				void 0,
-				new Date(Date.now() - 30 * 24 * 60 * 60 * 1000), // 30 days ago
-				new Date(Date.now()),
-				void 0,
-				void 0,
-				void 0,
-				void 0,
-				void 0,
-				void 0,
-				0,
-				25,
-				"date_created",
-				"desc",
-			);
-			const gameProps: Array<GameProps> = new Array(resp.data.length);
-			for (const game of resp.data) {
-				gameProps.push({
-					name: game.name,
-					id: game.id,
-					date: game.dateCreated,
-					rating: game.ratings.rating,
-					difficulty: game.ratings.difficulty,
-					numOfRatings: game.rating_count,
-				});
-			}
-			setGames(gameProps);
-		})();
-	}, []);
+		if (games.length === 0) {
+			(async () => {
+				let resp = await COMPOSITEAPICLIENT.getGamesWithRatings(
+					void 0,
+					void 0,
+					false,
+					void 0,
+					void 0,
+					void 0,
+					void 0,
+					void 0,
+					new Date(Date.now() - 30 * 24 * 60 * 60 * 1000), // 30 days ago
+					new Date(Date.now()),
+					void 0,
+					void 0,
+					void 0,
+					void 0,
+					void 0,
+					void 0,
+					0,
+					25,
+					"date_created",
+					"desc",
+				);
+				const gameProps: Array<GameProps> = new Array(resp.data.length);
+				for (const game of resp.data) {
+					gameProps.push({
+						name: game.name,
+						id: game.id,
+						date: game.dateCreated,
+						rating: game.ratings.rating,
+						difficulty: game.ratings.difficulty,
+						numOfRatings: game.rating_count,
+					});
+				}
+				setGames(gameProps);
+			})();
+		} else {
+			(async () => {
+				let resp = await REVIEWSAPICLIENT.getReviews(0, 5);
+				setReviews(resp.data);
+			})();
+		}
+	}, [games, reviews]);
 
 	return (
 		<div>
@@ -64,7 +79,7 @@ export default function Home(): JSX.Element {
 				<div id="content">
 					<News />
 					<GameList games={games} />
-					<ReviewList />
+					<ReviewList reviews={reviews} />
 				</div>
 			</div>
 		</div>
