80c3eba1dc69d05bd0b3f64cd4a39db5c0ef324f
Starz0r <starz0r@starz0r.com>
2025-08-18 23:07:43 -0400

    pages/home: display actual games from server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,36 @@ import Header from "../components/header";
 import News from "../components/home/news";
 import GameList from "../components/home/gameList";
 import ReviewList from "../components/home/reviewList";
+import { CompositeApi } from "delfruit-swagger-cg-sdk";
+import { useMemo, useState } from "react";
+import { GameProps } from "../components/game";
+
+const apiClient: CompositeApi = new CompositeApi(
+	void 0,
+	"http://localhost:4201",
+);
 
 export default function Home(): JSX.Element {
+	const [games, setGames] = useState<GameProps[]>([]);
+
+	useMemo(() => {
+		(async () => {
+			let resp = await apiClient.getGamesWithRatings();
+			const gameProps: Array<GameProps> = new Array(resp.data.length);
+			for (const game of resp.data) {
+				gameProps.push({
+					name: game.name,
+					id: game.id,
+					date: game.dateCreated,
+					rating: game.ratings.rating,
+					difficulty: game.ratings.difficulty,
+					numOfRatings: game.rating_count,
+				});
+			}
+			setGames(gameProps);
+		})();
+	}, []);
+
 	return (
 		<div>
 			<Head>
@@ -14,7 +42,7 @@ export default function Home(): JSX.Element {
 				<Header />
 				<div id="content">
 					<News />
-					<GameList />
+					<GameList games={games} />
 					<ReviewList />
 				</div>
 			</div>
