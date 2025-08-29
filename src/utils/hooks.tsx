e43425e66624275926f3939aeca3fb3dd0270e7d
Starz0r <starz0r@starz0r.com>
2025-08-28 20:15:00 -0400

    utils/hooks: useEffectAsync hook for asynchronous functions

--- src/utils/hooks.tsx
+++ src/utils/hooks.tsx
@@ -69,3 +69,84 @@ export function useSessionContext(): [
 ] {
 	return useContext(SessionContext);
 }
+
+export interface UseEffectAsyncResult {
+	result: any;
+	error: any;
+	isLoading: boolean;
+}
+/**
+ * Hook to run an effect asyncronously on mount and another on unmount.
+ * Taken from: https://marmelab.com/blog/2023/01/11/use-async-effect-react.html
+ * @param {() => Promise<any>} mountCallback - The callback to be ran on mount.
+ * @param {() => Promise<any>} unmountCallback - The callback to be ran on unmount.
+ * @param {any[]} deps - Dependencies that this effect relies on, causes a re-render if any of these changes.
+ * @returns UseEffectAsyncResult
+ */
+export function useEffectAsync(
+	mountCallback: () => Promise<any>,
+	unmountCallback: () => Promise<any>,
+	deps: any[] = [],
+): UseEffectAsyncResult {
+	const isMounted = useRef(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState<unknown>(undefined);
+	const [result, setResult] = useState<any>();
+
+	useEffect(() => {
+		isMounted.current = true;
+		return () => {
+			isMounted.current = false;
+		};
+	}, []);
+
+	useEffect(() => {
+		let ignore = false;
+		let mountSucceeded = false;
+
+		(async () => {
+			await Promise.resolve(); // wait for the initial cleanup in Strict mode - avoids double mutation
+			if (!isMounted.current || ignore) {
+				return;
+			}
+			setIsLoading(true);
+			try {
+				const result = await mountCallback();
+				mountSucceeded = true;
+				if (isMounted.current && !ignore) {
+					setError(undefined);
+					setResult(result);
+					setIsLoading(false);
+				} else {
+					// Component was unmounted before the mount callback returned, cancel it
+					unmountCallback();
+				}
+			} catch (error) {
+				if (!isMounted.current) return;
+				setError(error);
+				setIsLoading(false);
+			}
+		})();
+
+		return () => {
+			ignore = true;
+			if (mountSucceeded) {
+				unmountCallback()
+					.then(() => {
+						if (!isMounted.current) return;
+						setResult(undefined);
+					})
+					.catch((error: unknown) => {
+						if (!isMounted.current) return;
+						setError(error);
+					});
+			}
+		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, deps);
+
+	return useMemo(
+		() => ({ result, error, isLoading }),
+		[result, error, isLoading],
+	);
+}
