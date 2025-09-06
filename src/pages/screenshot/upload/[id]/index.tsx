5cd3af471d34c0ec940dc6f9f3229db6f994ee01
TTBB-Khai <khaiquocbui@gmail.com>
2025-09-06 04:37:50 -0400

    screenshot/upload: only allow logged in user to view page

--- src/pages/screenshot/upload/[id]/index.tsx
+++ src/pages/screenshot/upload/[id]/index.tsx
@@ -6,23 +6,27 @@ import { useEffect, useState } from "react";
 import { AnyElem } from "@/utils/element";
 import { useRouter } from "next/router";
 import { GamesApi } from "delfruit-swagger-cg-sdk";
+import { useSessionContext } from "@/utils/hooks";
 
 const CFG: Config = require("@/config.json");
 const GAMES_API_CLIENT = new GamesApi(undefined, CFG.apiURL.toString());
 
 export default function ScreenshotUploadPage(): AnyElem {
 	const [game, setGame] = useState<GameProps>();
+	const [error, setError] = useState<string | null>(null);
+	const [session] = useSessionContext();
 
 	const router = useRouter();
 
 	useEffect(() => {
 		if (!router.isReady) return;
+		setError(null);
 
 		const id = Number(router.query.id);
 		
 		// Anti-trolling measures
 		if (isNaN(id) || id < 0) {
-			router.replace({ pathname: "/screenshot/upload/[id]", query: { id: 0 } });
+			setError("Invalid page.")
 			return;
 		}
 
@@ -45,7 +49,13 @@ export default function ScreenshotUploadPage(): AnyElem {
 			<div id="container">
 				<Header />
 				<div id="content">
-					<UploadScreenshot game={game}/>
+					{error && <span className="text-red-600 ml-1">{error}</span>}
+					{!session.active && !error && (
+						<span>You must login to view this page.</span>
+					)}
+					{session.active && !error && (
+						<UploadScreenshot game={game}/>
+					)}
 				</div>
 				<Footer />
 			</div>
