5e9e1626fc9d5588bd253cdfda17c0585ddbd7f8
Starz0r <starz0r@starz0r.com>
2025-09-06 03:55:06 -0400

    components/review: button to report the related review

--- src/components/review.tsx
+++ src/components/review.tsx
@@ -3,13 +3,20 @@ import Link from "next/link";
 import React from "react";
 import { useState } from "react";
 import { Review as ReviewT } from "delfruit-swagger-cg-sdk";
+import { useSessionContext } from "@/utils/hooks";
 
 export default function Review(props: ReviewT): JSX.Element {
 	const [expanded, setExpanded] = useState(false);
+	const [session, setSession] = useSessionContext();
 
 	const maxLength = 500;
-	const shouldTruncate = props.comment ? props.comment.length > maxLength : false;
-	const displayText = expanded || !shouldTruncate ? props.comment : props.comment.slice(0, maxLength) + "...";
+	const shouldTruncate = props.comment
+		? props.comment.length > maxLength
+		: false;
+	const displayText =
+		expanded || !shouldTruncate
+			? props.comment
+			: props.comment.slice(0, maxLength) + "...";
 
 	return (
 		<div className={`review ${props.owner_review ? "owner-review" : ""}`}>
@@ -17,7 +24,6 @@ export default function Review(props: ReviewT): JSX.Element {
 			<Link href={`/user/${props.user_id}`}>{props.user_name}</Link>
 			{props.owner_review && <span className="!font-bold">[Creator]</span>}
 			<br />
-
 
 			{/* GAME NAME */}
 			{props.game_id && (
@@ -29,7 +35,7 @@ export default function Review(props: ReviewT): JSX.Element {
 			)}
 
 			{/* COMMENT */}
-			{(props.comment !== "" && props.comment !== null) && (
+			{props.comment !== "" && props.comment !== null && (
 				<div>
 					<div className="review-text !wrap-break-word">
 						<span>{displayText}</span>
@@ -67,29 +73,36 @@ export default function Review(props: ReviewT): JSX.Element {
 				<span className="!align-middle mr-[0.5em]">
 					Rating: {props.rating === null ? "N/A" : `${props.rating}`}
 				</span>
-				{
-					props.rating !== null ? (
-						<span className="hearts">
-							<span style={{ width: `${170 * (props.rating / 10)}px` }}/>
-						</span>
+				{props.rating !== null ? (
+					<span className="hearts">
+						<span style={{ width: `${170 * (props.rating / 10)}px` }} />
+					</span>
 				) : (
-					<span className="inline-block w-[170px] h-[16px]"/>
+					<span className="inline-block w-[170px] h-[16px]" />
 				)}
 				{/* DIFFICULTY */}
 				<span className="!align-middle ml-[2em] mr-[0.5em]">
-					Difficulty: {props.difficulty === null ? "N/A" : `${props.difficulty}`}
+					Difficulty:{" "}
+					{props.difficulty === null ? "N/A" : `${props.difficulty}`}
 				</span>
-				{
-					props.difficulty !== null ? (
-						<span className="stars">
-							<span style={{ width: `${170 * (props.difficulty / 100)}px` }}/>
-						</span>
+				{props.difficulty !== null ? (
+					<span className="stars">
+						<span style={{ width: `${170 * (props.difficulty / 100)}px` }} />
+					</span>
 				) : (
-					<span className="inline-block w-[170px] h-[16px]"/>
+					<span className="inline-block w-[170px] h-[16px]" />
 				)}
 				{/* DATE */}
 				<div className="!absolute !right-[0px] !top-[0px] !p-[0.5em] !text-right">
 					{props.date_created}
+
+					{/* Buttons */}
+					{session.active && (
+						<>
+							{" "}
+							<Link href={`/report/${props.id}`}>Report</Link>
+						</>
+					)}
 				</div>
 			</div>
 		</div>
