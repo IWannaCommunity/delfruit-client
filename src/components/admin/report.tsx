fd13e0b860177e35a584ccdfa2250e2da17fb671
TTBB-Khai <khaiquocbui@gmail.com>
2026-01-29 02:02:44 -0500

    components/admin/report: report type links added

--- src/components/admin/report.tsx
+++ src/components/admin/report.tsx
@@ -1,11 +1,46 @@
 import Link from "next/link";
 import React from "react";
 import { Report as ReportT } from "delfruit-swagger-cg-sdk";
+import { ReportTypeEnum } from "delfruit-swagger-cg-sdk";
 import { formatDate } from "@/utils/formatDate";
 
 type ReportProps = ReportT & {
 	onResolve: () => void;
 };
+
+function getReportTargetHref(
+	type: ReportTypeEnum,
+	targetId?: number | null
+): string {
+	if (!targetId) return "#";
+
+	switch (type) {
+		case ReportTypeEnum.Review:
+		case ReportTypeEnum.ReviewRestore:
+			return `/review/${targetId}`;
+
+		case ReportTypeEnum.Game:
+		case ReportTypeEnum.GameAdd:
+		case ReportTypeEnum.GameRemove:
+		case ReportTypeEnum.GameUpdate:
+		case ReportTypeEnum.GameUpdateUrl:
+		case ReportTypeEnum.GameUpdateOwner:
+		case ReportTypeEnum.GameUpdateCreator:
+			return `/game/${targetId}`;
+
+		case ReportTypeEnum.Screenshot:
+		case ReportTypeEnum.ScreenshotRemove:
+			return `/screenshot/${targetId}`;
+
+		case ReportTypeEnum.User:
+		case ReportTypeEnum.UserRegister:
+		case ReportTypeEnum.UserPasswordChange:
+			return `/profile/${targetId}`;
+
+		default:
+			return "#";
+	}
+}
 
 export default function Report(props: ReportProps): JSX.Element {
 	const isAnswered = Boolean(props.dateAnswered);
@@ -19,7 +54,9 @@ export default function Report(props: ReportProps): JSX.Element {
 			<span>Reported on: {props.dateCreated}</span>
 			<br />
 			<span>Reported Type: </span>
-			<Link href="/">{props.type}</Link>
+			<Link href={getReportTargetHref(props.type, props.targetId)}>
+				{props.type}
+			</Link>
 			<br />
 			<br />
 			<span>Report: {props.report}</span>
