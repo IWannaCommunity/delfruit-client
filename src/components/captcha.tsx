b86b2d70f939f6d6fa81f51a119bce42d52d89de
Starz0r <starz0r@starz0r.com>
2026-03-15 11:36:56 -0400

    components/captcha: container for captcha widgets

---
+++ src/components/captcha.tsx
@@ -0,0 +1,13 @@
+import { Turnstile } from "@marsidev/react-turnstile";
+import { CFG } from "@/utils/config";
+import type { AnyElem } from "@/utils/element";
+
+export default function Captcha(onSuccess: (token: string) => void): AnyElem {
+	return (
+		<Turnstile
+			siteKey={CFG.cfTurnstileSiteKey}
+			rerenderOnCallbackChange={true}
+			onSuccess={onSuccess}
+		/>
+	);
+}
