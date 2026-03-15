2543905e7d5e3a0c35804fe4c7054e0442a4b8f8
Starz0r <starz0r@starz0r.com>
2026-03-15 11:39:27 -0400

    utils/config: parse for cloudflare turnstile site key field

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,9 +3,12 @@ import rawConfig from "@/config.json";
 export interface Config {
 	apiURL: URL;
 	screenshotURLPrefix: URL;
+	cfTurnstileSiteKey: string;
 }
 
 export const CFG: Config = {
-  apiURL: new URL(rawConfig.apiURL),
-  screenshotURLPrefix: new URL(rawConfig.screenshotURLPrefix),
+	apiURL: new URL(rawConfig.apiURL),
+	screenshotURLPrefix: new URL(rawConfig.screenshotURLPrefix),
+	cfTurnstileSiteKey: rawConfig.cfTurnstileSiteKey,
 };
+
