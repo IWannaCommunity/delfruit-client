9cf7d541460ecac5b6e6b66178e104fe70ec4f68
Starz0r <starz0r@starz0r.com>
2026-04-05 15:45:30 -0400

    pages/.lost-and-found: attempt to maintain old style of url paths

--- src/pages/.lost-and-found/index.tsx
+++ src/pages/.lost-and-found/index.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
-import { AnyElem } from "@/utils/element";
-import { useRouter } from "next/router";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import type { AnyElem } from "@/utils/element";
 
 export default function LostAndFound(): AnyElem {
 	const router = useRouter();
@@ -11,8 +11,18 @@ export default function LostAndFound(): AnyElem {
 	const desiredPath = router.asPath;
 
 	useEffect(() => {
+		if (desiredPath.includes("/ratings/game_details.php?id=")) {
+			const gameId = desiredPath.split("/ratings/game_details.php?id=")[1];
+			return router.push(`/game/${gameId}`);
+		} else if (desiredPath.includes("/profile.php?u=")) {
+			const userId = desiredPath.split("/profile.php?u=")[1];
+			return router.push(`/profile/${userId}`);
+		} else if (desiredPath.includes("/news.php?id=")) {
+			const newsId = desiredPath.split("/news.php?id=")[1];
+			return router.push(`/news/${newsId}`);
+		}
 		router.push(`${desiredPath}`);
-	}, [router, pathname, desiredPath]);
+	}, [router, desiredPath]);
 
 	return (
 		<>
