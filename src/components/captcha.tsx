e69f59d7d951e4fca4ba89906e1bdc913ef57ea1
TTBB-Khai <khaiquocbui@gmail.com>
2026-04-16 21:44:57 -0400

    components/captcha: fix onSuccess typeError

--- src/components/captcha.tsx
+++ src/components/captcha.tsx
@@ -2,7 +2,11 @@ import { Turnstile } from "@marsidev/react-turnstile";
 import { CFG } from "@/utils/config";
 import type { AnyElem } from "@/utils/element";
 
-export default function Captcha(onSuccess: (token: string) => void): AnyElem {
+type CaptchaProps = {
+	onSuccess: (token: string) => void;
+}
+
+export default function Captcha({onSuccess}: CaptchaProps): AnyElem {
 	return (
 		<Turnstile
 			siteKey={CFG.cfTurnstileSiteKey}
